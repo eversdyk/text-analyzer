a91ed5a3283102101daf1fdda3bce9f3c4f2a980
Cliff Eversdyk <eversdyk@gmail.com>
2017-03-07 15:42:28 -0600

    added a README file and made some small edits to the code in text-analyzer.py

---
+++ README.md
@@ -0,0 +1,11 @@
+This challenge was to create a simple text content analyzer. This is a tool used by writers to find statistics such as word and sentence count on essays or articles they are writing.
+
+The program should output:
+1. The total word count
+2. The count of unique words
+3. The number of sentences
+
+Example output:
+Total word count: 468
+Unique words: 223
+Sentences: 38
--- text_analyzer.py
+++ text_analyzer.py
@@ -1,21 +1,3 @@
-"""
-The challenge is to create a text content analyzer. This is a tool used by writers to find statistics such as word and sentence count on essays or articles they are writing.
-Write a Python program that analyzes input from a file and compiles statistics on it.
-The program should output:
-1. The total word count
-2. The count of unique words
-3. The number of sentences
-Example output:
-
-Total word count: 468
-Unique words: 223
-Sentences: 38
-Brownie points will be awarded for the following extras:
-1. The ability to calculate the average sentence length in words
-2. The ability to find often used phrases (a phrase of 3 or more words used over 3 times)
-3. A list of words used, in order of descending frequency
-4. The ability to accept input from STDIN, or from a file specified on the command line.
-"""
 import re
 import string
 def textcontent(filename):
@@ -35,4 +17,5 @@ def textcontent(filename):
     print("Number of sentences: %i" %(sentencecount))
     print("Most frequently occurring word: '%s'" %(max(wordfreq, key=wordfreq.get)))
 
+#tested with a passage from Lord of the Rings :)
 textcontent("LOTR.txt")
